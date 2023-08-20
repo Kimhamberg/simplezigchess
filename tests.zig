d63000a3cb749cc5e4c205c21460d7a066421908
Kim Hamberg <15271951+Kimhamberg@users.noreply.github.com>
2023-08-20 18:01:43 +0200

    fixed two-up pawn move checks if space between and added test file

---
+++ tests.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const assert = std.testing.assert;
+const MoveGenerator = @import("generation.zig").MoveGenerator;
+const setupBoard = @import("generation.zig").setupBoard;
+const 
+const Color = @import("generation.zig").Color;
+const Move = @import("generation.zig").Move;
+
+var moveGenerator = MoveGenerator{ .board = undefined, .iMove = undefined, .playerColor = Color.White, .playerMoves = [_]Move{undefined} ** 256 };
+
+test "Ensure valid moves for pawn" {
+    
+    // expect(moveGenerator.playerMoves[0].from == some_expected_value);
+}
+
+test "Ensure valid moves for rook" {
+    // Similar structure as above. Initialize MoveGenerator, call function, check results.
+}
+
+// ... Write more tests for other pieces ...
